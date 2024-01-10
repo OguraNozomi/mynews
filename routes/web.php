1a53acd8b42998e1b9f9476ba662689b9e7c6bf2
Ogura <non01211983@yahoo.co.jp>
2024-01-10 05:16:14 +0000

    php08_0110

--- routes/web.php
+++ routes/web.php
@@ -18,17 +18,22 @@ Route::get('/', function () {
 });
 
 use App\Http\Controllers\Admin\NewsController;
-Route::controller(NewsController::class)->prefix('admin')->group(function() {
-    Route::get('news/create', 'add')->middleware('auth');
+Route::controller(NewsController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function(){
+    Route::get('news/create', 'add')->name('news.add');
+    Route::post('news/create', 'create')->name('news.create');
 });
+
 // ↑↑↑書き換える↓↓↓
 // use App\Http\Controllers\Admin\NewsController;
-// Route::get('/admin/news/create', [NewsController::class, 'add'])->middleware('auth');
+// Route::get('/admin/news/create', [NewsController::class, 'add'])->name('admin.news.add')->middleware('auth');
+// Route::post('/admin/news/create', [NewsController::class, 'create'])->name('admin.news.create')->middleware('auth');
 
 use App\Http\Controllers\Admin\ProfileController;
-Route::controller(ProfileController::class)->prefix('admin')->group(function() {
-    Route::get('profile/create', 'add')->middleware('auth');
-    Route::get('profile/edit', 'edit')->middleware('auth');
+Route::controller(ProfileController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function() {
+    Route::get('profile/create', 'add')->name('profile.add');
+    Route::post('profile/create', 'create')->name('profile.create');
+    Route::get('profile/edit', 'edit')->name('profile.edit');
+    Route::post('profile/edit', 'update')->name('profile.update');
 });
 
 Auth::routes();
