905b35933c191a9ad139690e870f005c53694597
Ogura <non01211983@yahoo.co.jp>
2024-01-16 06:19:20 +0000

    php11_0116

--- database/migrations/2024_01_11_021757_create_profiles_table.php
+++ database/migrations/2024_01_11_021757_create_profiles_table.php
@@ -4,9 +4,6 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-//DB利用追記
-use App\Models\News;
-
 return new class extends Migration
 {
     /**
